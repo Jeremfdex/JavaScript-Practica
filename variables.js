4769e240b501816e620b6eb19b109060a6cd42ec
jeremy <jeremy.garate@usach.cl>
2026-05-21 19:48:18 -0400

    nueva

--- variables.js
+++ variables.js
@@ -1,5 +1,5 @@
 let x = "Hola soy un string"; // variables que no cambian 
-const rut = 200593952; // const se usa cuando la variable no cambiara, por ejemplo una variable fisica o matematica como pi
-var apellido = "Garate"; // Js estandar moderno de declaracion de variables
+const rut = 343242; // const se usa cuando la variable no cambiara, por ejemplo una variable fisica o matematica como pi
+var apellido = "XD"; // Js estandar moderno de declaracion de variables
 
 console.log(x)
